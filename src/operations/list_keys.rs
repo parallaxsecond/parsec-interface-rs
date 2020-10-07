c79591ecdd7d98a08d64f5bbda17dc429e9b4915
Joe Ellis <joe.ellis@arm.com>
2020-10-07 09:44:26 +0100

    Add Rust-native types for ListKeys operation
    Signed-off-by: Joe Ellis <joe.ellis@arm.com>

---
+++ src/operations/list_keys.rs
@@ -0,0 +1,28 @@
+// Copyright 2020 Contributors to the Parsec project.
+// SPDX-License-Identifier: Apache-2.0
+//! # ListKeys operation
+//!
+//! Lists all keys belonging to the application.
+use super::psa_key_attributes::Attributes;
+
+/// Structure holding the basic information for a key in the application for client discovery.
+#[derive(Debug, Clone, PartialEq)]
+pub struct KeyInfo {
+    /// The ID of the associated provider.
+    pub provider_id: u32,
+    /// The name of the key.
+    pub name: String,
+    /// The key attributes.
+    pub attributes: Attributes,
+}
+
+/// Native object for key listing operation.
+#[derive(Copy, Clone, Debug)]
+pub struct Operation;
+
+/// Native object for key listing result.
+#[derive(Debug)]
+pub struct Result {
+    /// A list of `KeyInfo` structures.
+    pub keys: Vec<KeyInfo>,
+}
